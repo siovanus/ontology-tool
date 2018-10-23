a38c203568d6bc673307b00bf73fa46cdfe98fe3
zhangmenghang <zhangmenghang@onchain.com>
2018-10-23 04:44:02 +0100

    add side chain tool

--- testcase/smartcontract/native/init.go
+++ testcase/smartcontract/native/init.go
@@ -2,11 +2,13 @@ package native
 
 import (
 	"github.com/ontio/ontology-tool/testcase/smartcontract/native/governance_feeSplit"
+	"github.com/ontio/ontology-tool/testcase/smartcontract/native/side_chain_governance"
 )
 
 func TestNative() {
 	//governance_feeSplit.TestGovernanceContract()
 	//governance_feeSplit.TestGovernanceContractError()
 	governance_feeSplit.TestGovernanceMethods()
+	side_chain_governance.TestGovernanceMethods()
 	//governance_feeSplit.TestGovernanceBatch()
 }
---
+++ testcase/smartcontract/native/side_chain_governance/init.go
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package side_chain_governance
+
+import (
+	"github.com/ontio/ontology-tool/testframework"
+)
+
+func TestGovernanceMethods() {
+	testframework.TFramework.RegTestCase("RegisterSideChain", RegisterSideChain)
+	testframework.TFramework.RegTestCase("ApproveSideChain", ApproveSideChain)
+	testframework.TFramework.RegTestCase("RejectSideChain", RejectSideChain)
+	testframework.TFramework.RegTestCase("RegisterNodeToSideChain", RegisterNodeToSideChain)
+	testframework.TFramework.RegTestCase("GetSideChain", GetSideChain)
+	testframework.TFramework.RegTestCase("GetSideChainNodeInfo", GetSideChainNodeInfo)
+}
